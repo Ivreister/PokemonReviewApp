a7a5a9cb9994ec48f3faaf7efa57e13f5d16563e
Ivreister <reversbor@gmail.com>
2025-03-15 11:37:46 +0200

    Lesson 8 It works

--- Helper/MappingProfiles.cs
+++ Helper/MappingProfiles.cs
@@ -13,7 +13,7 @@ namespace PokemonReviewApp.Helper
             CreateMap<Country, CountryDto>();
             CreateMap<Owner, OwnerDto>();
             CreateMap<Review, ReviewDto>();
-            CreateMap<Reviewer, ReviewDto>();
+            CreateMap<Reviewer, ReviewerDto>();
 
         }
     }
