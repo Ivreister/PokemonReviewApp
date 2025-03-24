605c56fea18f282ded460d707140dc4a4b62f6f2
Ivreister <reversbor@gmail.com>
2025-03-24 17:50:10 +0200

    11 Lesson 32 min

--- Controllers/ReviewerController.cs
+++ Controllers/ReviewerController.cs
@@ -97,5 +97,39 @@ namespace PokemonReviewApp.Controllers
 
             return Ok("Successfuly created");
         }
+
+        [HttpPut("{reviewerId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult UpdateReviewer(int reviewerId, [FromBody] ReviewerDto updatedReviewer)
+        {
+            if (updatedReviewer == null)
+            {
+                return BadRequest(ModelState);
+            }
+            if (reviewerId != updatedReviewer.Id)
+            {
+                return BadRequest(ModelState);
+            }
+            if (!_reviewerRepository.ReviewerExists(reviewerId))
+            {
+                return NotFound();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var reviewerMap = _mapper.Map<Reviewer>(updatedReviewer);
+
+            if (!_reviewerRepository.UpdateReviewer(reviewerMap))
+            {
+                ModelState.AddModelError("", "Somthing went wrong updating reviewer");
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok("Successfuly created");
+        }
     }
 }
