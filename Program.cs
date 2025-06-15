029c5e9de34612e7441584c222c75ed4726d4e8b
Ivreister <reversbor@gmail.com>
2025-06-15 18:37:59 +0300

    Test

--- Program.cs
+++ Program.cs
@@ -14,19 +14,15 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration["ConnectionStrings:DefaultConnection"]
     ?? throw new Exception("Connection string is missing!");
 
-Console.WriteLine($"Using DB: {connectionString}");
-
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseNpgsql(connectionString, npgsqlOptions =>
-        npgsqlOptions.EnableRetryOnFailure()
-    ));
+        npgsqlOptions.EnableRetryOnFailure()));
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddTransient<Seed>();
-builder.Services.AddControllers().AddJsonOptions(x =>
-                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
@@ -34,38 +30,12 @@ builder.Services.AddScoped<ICountryRepository, CountryRepository>();
 builder.Services.AddScoped<IOwnerRepository, OwnerRepository>();
 builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
 builder.Services.AddScoped<IReviewerRepository, ReviewerRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors(options => {
-    options.AddPolicy("AllowAll", builder => {
-        builder.AllowAnyOrigin()
-               .AllowAnyMethod()
-               .AllowAnyHeader();
-    });
-});
-
-Console.WriteLine("=== ENV VARIABLES ===");
-foreach (var envVar in Environment.GetEnvironmentVariables().Cast<DictionaryEntry>())
-{
-    Console.WriteLine($"{envVar.Key}: {envVar.Value}");
-}
 
 var app = builder.Build();
-
-if (args.Length == 1 && args[0].ToLower() == "seeddata")
-    SeedData(app);
-
-void SeedData(IHost app)
-{
-    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
-
-    using (var scope = scopedFactory.CreateScope())
-    {
-        var service = scope.ServiceProvider.GetService<Seed>();
-        service.SeedDataContext();
-    }
-}
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
@@ -73,21 +43,8 @@ app.UseSwaggerUI(c => {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pokemon API v1");
 });
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 app.UseAuthorization();
-app.UseCors("AllowAll");
-app.MapGet("/db-debug", (DataContext context) =>
-{
-    try
-    {
-        var canConnect = context.Database.CanConnect();
-        return $"DB Connection: {canConnect}";
-    }
-    catch (Exception ex)
-    {
-        return $"DB Error: {ex.Message}";
-    }
-});
 
 using (var scope = app.Services.CreateScope())
 {
